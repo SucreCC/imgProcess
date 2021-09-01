342849900886d484add6c6e22fc31e1148a77224
dengkai <dengkai@wxchina.com>
2021-09-01 18:11:28 +0800

    imgProcess

---
+++ imgProcess/src/test/java/com/imgprocess/ImgProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imgprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
